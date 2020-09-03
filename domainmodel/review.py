178d7558ec3c46d62ca8c1dda5113754b46d40fc
Han <morrs456@gmail.com>
2020-09-04 03:03:16 +1200

    Q6

--- domainmodel/review.py
+++ domainmodel/review.py
@@ -2,5 +2,37 @@ from datetime import datetime
 
 from domainmodel.movie import Movie
 
+
 class Review:
-    pass
+    def __init__(self, input_movie: Movie, review_text: str, input_rating: int):
+        if input_rating < 0 or input_rating > 10:
+            self.__rating = None
+        else:
+            self.__reviewText = review_text
+            self.__rating = input_rating
+            now = datetime.now()
+            self.__timestamp = datetime.timestamp(now)
+            self.__movie = input_movie
+
+    def __repr__(self):
+        return self.__movie + f"Review: {self.__reviewText}, Rating: {self.__rating}"
+
+    def __eq__(self, other):
+        return (self.__rating == other.__rating) and (self.__movie == other.__movie) \
+               and (self.__timestamp == other.__timestamp) and (self.__reviewText == other.__reviewText)
+
+    @property
+    def movie(self):
+        return self.__movie
+
+    @property
+    def review_text(self):
+        return  self.__reviewText
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @property
+    def timestamp(self):
+        return self.__timestamp
